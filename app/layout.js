f72e96320ff18c77ebb999f887066f92a6448274
Leoy3 <157263801+Leoy3@users.noreply.github.com>
2026-07-08 10:03:33 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,12 @@ import "./globals.css";
 
 export const metadata = {
   title: "Brains Counter Stream",
-  description: "Countdown until the next eatfreshbrains stream"
+  description: "Countdown until the next eatfreshbrains stream",
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png"
+  }
 };
 
 export default function RootLayout({ children }) {
