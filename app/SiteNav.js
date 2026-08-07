d9eca7a0d7b634ebb25b93e54ffbf461e55fae46
Leoy3 <157263801+Leoy3@users.noreply.github.com>
2026-08-07 00:03:27 -0300

    Update SiteNav.js

--- app/SiteNav.js
+++ app/SiteNav.js
@@ -109,12 +109,10 @@ export default function SiteNav() {
           aria-label="VGen"
           title="VGen"
         >
-          <svg viewBox="0 0 24 24" aria-hidden="true">
-            <path
-              fill="currentColor"
-              d="M2.8 4.2h4.4L12 16.1l4.8-11.9h4.4L14.2 20h-4.4L2.8 4.2Z"
-            />
-          </svg>
+          <MaskIcon
+            url="https://upload.wikimedia.org/wikipedia/commons/6/6c/VGen_icon_-_filled.png"
+            size={19}
+          />
         </a>
 
         <a
